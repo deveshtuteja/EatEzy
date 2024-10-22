1627e1eb002cbb27b1c54383d43262ef8d2a8a46
deveshtuteja <deveshtuteja17@gmail.com>
2024-10-22 20:24:46 +0530

    footer enhanced

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <div className="p-4 bg-pink-100 text-center font-bold hover:bg-pink-200 hover:font-extrabold">
+        <div className="p-4 bg-pink-100 text-center font-bold hover:bg-pink-200 hover:font-extrabold shadow-inner">
             <h3>Made with Love by <a href="https://www.linkedin.com/in/devesh-tuteja/">Devesh Tuteja</a>❤️</h3>
             <h4>©️EatEzy-2024</h4>
         </div>
